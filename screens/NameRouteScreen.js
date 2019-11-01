a6de440b262330e15b91960a7d9346853087cc07
Liam Duncan <liam.duncan310@gmail.com>
2019-11-01 16:24:51 +0000

    implement MapView on subsequent question screens and pass route up HomeStack

--- screens/NameRouteScreen.js
+++ screens/NameRouteScreen.js
@@ -1,5 +1,13 @@
 import React from 'react';
-import { Text, TextInput, View, Button } from 'react-native';
+import {
+  Text,
+  TextInput,
+  View,
+  Dimensions,
+  TouchableHighlight
+} from 'react-native';
+import MapView, { Polyline } from 'react-native-maps';
+import Constants from 'expo-constants';
 
 export default class NameRouteScreen extends React.Component {
   state = {
@@ -12,30 +20,110 @@ export default class NameRouteScreen extends React.Component {
       <View
         style={{
           flex: 1,
+          alignItems: 'center',
           justifyContent: 'center',
-          alignItems: 'center'
+          paddingTop: Constants.statusBarHeight,
+          backgroundColor: '#ffffff'
         }}
       >
+        <MapView
+          style={{
+            width: Dimensions.get('window').width,
+            // height: Dimensions.get('window').height
+            flex: 1
+          }}
+          initialRegion={{
+            latitude: 53.79493492583547,
+            longitude: -1.546191464587611,
+            latitudeDelta: 0.0073,
+            longitudeDelta: 0.0073
+            // latitudeDelta: 0.0922,
+            // longitudeDelta: 0.0421
+          }}
+        >
+          <Polyline
+            coordinates={navigation.getParam('actualRoute', [])}
+            strokeColor="#000000"
+            strokeWidth={6}
+          />
+        </MapView>
+        {/* <Text>Question 1!</Text>
+        <Text>route: {navigation.getParam('actualRoute', [])}</Text> */}
+        <View
+          style={{
+            position: 'absolute',
+            top: 15 + Constants.statusBarHeight,
+            borderRadius: 5,
+            backgroundColor: '#3cc1c7',
+            marginLeft: 15,
+            marginRight: 15,
+            paddingLeft: 15,
+            paddingRight: 15,
+            paddingTop: 10,
+            paddingBottom: 10
+          }}
+        >
+          <Text style={{ color: 'white', fontSize: 20 }}>
+            Please enter a name for your route :)
+          </Text>
+        </View>
         <TextInput
           style={{
             height: 40,
-            width: 120,
-            justifyContent: 'flex-start'
+            width: 300,
+            // justifyContent: 'flex-start',
+            position: 'absolute',
+            top: 75 + Constants.statusBarHeight,
+            backgroundColor: 'white',
+            color: 'black'
           }}
           placeholder="Name your route"
           onChangeText={actualRouteName => this.setState({ actualRouteName })}
           value={this.state.actualRouteName}
         />
-        <Button
-          title="Submit your route"
+        <View
+          style={{
+            position: 'absolute',
+            bottom: 25,
+            borderRadius: 5,
+            backgroundColor: '#848484',
+            marginLeft: 15,
+            marginRight: 15,
+            paddingLeft: 15,
+            paddingRight: 15,
+            paddingTop: 10,
+            paddingBottom: 10
+          }}
+        >
+          <Text style={{ color: 'white', fontSize: 18 }}>
+            Press done to submit your route
+          </Text>
+        </View>
+        <TouchableHighlight
           onPress={() => {
-            this.props.navigation.navigate('ConfirmRouteAdded', {
-              actualRoute: navigation.getParam('actualRoute'),
-              actualRouteName: this.state.actualRouteName,
-              flags: this.state.flags
+            navigation.navigate('ConfirmRouteAdded', {
+              actualRouteName: this.state.actualRouteName
             });
           }}
-        />
+          style={{
+            position: 'absolute',
+            bottom: 125,
+            borderRadius: 500
+          }}
+        >
+          <View
+            style={{
+              backgroundColor: '#848484',
+              borderRadius: 50000,
+              paddingLeft: 35,
+              paddingRight: 35,
+              paddingTop: 15,
+              paddingBottom: 15
+            }}
+          >
+            <Text style={{ color: 'white', fontSize: 32 }}>DONE</Text>
+          </View>
+        </TouchableHighlight>
         {this.state.flags.map((flag, index) => {
           return <Text key={index}>{flag} </Text>;
         })}
--- screens/SecondQuestionScreen.js
+++ screens/SecondQuestionScreen.js
@@ -1,5 +1,7 @@
 import React from 'react';
-import { Text, View, Button } from 'react-native';
+import { Text, View, Dimensions, TouchableHighlight } from 'react-native';
+import MapView, { Polyline } from 'react-native-maps';
+import Constants from 'expo-constants';
 
 export default class SecondQuestionScreen extends React.Component {
   state = {
@@ -11,29 +13,106 @@ export default class SecondQuestionScreen extends React.Component {
       <View
         style={{
           flex: 1,
+          alignItems: 'center',
           justifyContent: 'center',
-          alignItems: 'center'
+          paddingTop: Constants.statusBarHeight,
+          backgroundColor: '#ffffff'
         }}
       >
-        <Text>Question 2!</Text>
-        <Text>route: {navigation.getParam('actualRoute', 'N/A')}</Text>
-        <Button
-          title="Go to q3"
+        <MapView
+          style={{
+            width: Dimensions.get('window').width,
+            // height: Dimensions.get('window').height
+            flex: 1
+          }}
+          initialRegion={{
+            latitude: 53.79493492583547,
+            longitude: -1.546191464587611,
+            latitudeDelta: 0.0073,
+            longitudeDelta: 0.0073
+            // latitudeDelta: 0.0922,
+            // longitudeDelta: 0.0421
+          }}
+        >
+          <Polyline
+            coordinates={navigation.getParam('actualRoute', [])}
+            strokeColor="#000000"
+            strokeWidth={6}
+          />
+        </MapView>
+        {/* <Text>Question 1!</Text>
+        <Text>route: {navigation.getParam('actualRoute', [])}</Text> */}
+        <View
+          style={{
+            position: 'absolute',
+            top: 15 + Constants.statusBarHeight,
+            borderRadius: 5,
+            backgroundColor: '#3cc1c7',
+            marginLeft: 15,
+            marginRight: 15,
+            paddingLeft: 15,
+            paddingRight: 15,
+            paddingTop: 10,
+            paddingBottom: 10
+          }}
+        >
+          <Text style={{ color: 'white', fontSize: 20 }}>
+            Please mark any areas with inadequate lighting on the route below
+          </Text>
+        </View>
+        <View
+          style={{
+            position: 'absolute',
+            bottom: 25,
+            borderRadius: 5,
+            backgroundColor: '#848484',
+            marginLeft: 15,
+            marginRight: 15,
+            paddingLeft: 15,
+            paddingRight: 15,
+            paddingTop: 10,
+            paddingBottom: 10
+          }}
+        >
+          <Text style={{ color: 'white', fontSize: 18 }}>
+            Press and drag on the route to place a marker, then press next to move
+            onto the next question
+          </Text>
+        </View>
+        <TouchableHighlight
           onPress={() => {
-            this.props.navigation.navigate('ThirdQuestion', {
+            navigation.navigate('ThirdQuestion', {
               actualRoute: navigation.getParam('actualRoute'),
               flags: this.state.flags
             });
           }}
-        />
-        <Button
-          title="Add light flag"
+          style={{
+            position: 'absolute',
+            bottom: 125,
+            borderRadius: 500
+          }}
+        >
+          <View
+            style={{
+              backgroundColor: '#848484',
+              borderRadius: 50000,
+              paddingLeft: 35,
+              paddingRight: 35,
+              paddingTop: 15,
+              paddingBottom: 15
+            }}
+          >
+            <Text style={{ color: 'white', fontSize: 32 }}>></Text>
+          </View>
+        </TouchableHighlight>
+        {/* <Button
+          title="Add mud flag"
           onPress={() => {
             this.setState(currentState => {
-              return { flags: [...currentState.flags, 'light'] };
+              return { flags: [...currentState.flags, 'mud'] };
             });
           }}
-        />
+        /> */}
         {this.state.flags.map((flag, index) => {
           return <Text key={index}>{flag} </Text>;
         })}
