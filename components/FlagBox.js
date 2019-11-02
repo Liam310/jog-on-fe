32db674111a0f5dca58725019e539eaf3f3ac490
Simon Green <simonisaacgreen@icloud.com>
2019-11-02 16:32:59 +0000

    add FlagBox and RouteCard components for the routes list

---
+++ components/FlagBox.js
@@ -0,0 +1,37 @@
+import React from 'react';
+import { View, Image } from 'react-native';
+import styled from 'styled-components';
+import flagRef from '../utils/flagRefObj';
+
+const FlagView = styled.View`
+  flex-direction: row;
+  background-color: #c4c4c4;
+  border-radius: 3;
+  margin-right: 10;
+  justify-content: space-around;
+`;
+
+const FlagImage = styled.Image`
+  width: 20;
+  height: 20;
+  margin-right: 1;
+  margin-left: 1;
+  margin-top: 3;
+  margin-bottom: 2;
+`;
+
+const FlagBox = ({ flags }) => {
+  const flagsCondensed = Array.from(new Set(flags));
+  const styleObj = { width: flagsCondensed.length * 22 };
+
+  return (
+    <>
+      <FlagView style={styleObj}>
+        {flagsCondensed.map((flag, index) => (
+          <FlagImage key={`${index}FlagIcon`} source={flagRef[flag]} />
+        ))}
+      </FlagView>
+    </>
+  );
+};
+export default FlagBox;
