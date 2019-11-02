5f4fbf71d1b2cc5c84b0e12f47116f23f855a204
Simon Green <simonisaacgreen@icloud.com>
2019-11-02 12:34:53 +0000

    adding api file

---
+++ utils/api.js
@@ -0,0 +1,17 @@
+import * as axios from 'axios';
+
+const request = axios.create({
+  baseURL: 'https://judsfg9bq1.execute-api.eu-west-2.amazonaws.com/latest/api'
+  // baseURL: 'http://172.26.44.25:3000/api'
+});
+
+export const postFlags = flags => {
+  return request
+    .post('/flags', flags)
+    .then(({ data }) => {
+      console.log(data);
+    })
+    .catch(error => {
+      console.log(error);
+    });
+};
