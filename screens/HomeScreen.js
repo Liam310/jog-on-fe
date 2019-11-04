844f773d514b088622e55689be3eaac533494e73
Simon Green <simonisaacgreen@icloud.com>
2019-11-04 15:44:37 +0000

    update start button styling so that colour changes on press

--- screens/HomeScreen.js
+++ screens/HomeScreen.js
@@ -109,17 +109,24 @@ export default class HomeScreen extends React.Component {
           style={{
             position: 'absolute',
             bottom: 25,
-            borderRadius: 50000
+            borderRadius: 500
           }}
         >
           <View
             style={{
-              backgroundColor: '#3cc1c7',
-              borderRadius: 50000,
+              backgroundColor: this.state.gettingLocation
+                ? '#FF3D3D'
+                : '#3cc1c7',
+              borderRadius: 500,
               paddingLeft: 35,
               paddingRight: 35,
               paddingTop: 15,
-              paddingBottom: 15
+              paddingBottom: 15,
+              width: 150,
+              height: 60,
+              flex: 1,
+              alignItems: 'center',
+              justifyContent: 'center'
             }}
           >
             <Text style={{ color: 'white', fontSize: 20 }}>
