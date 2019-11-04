c267aa208781930fbf3a30432baf611a3ea32e48
Simon Green <simonisaacgreen@icloud.com>
2019-11-04 15:09:47 +0000

    remove console logs

--- utils/api.js
+++ utils/api.js
@@ -19,8 +19,6 @@ export const getFlags = async regionQueryObj => {
 
 // ROUTES
 export const getRoutes = async ({ user_lat, user_long }) => {
-  console.log(user_lat);
-  console.log(user_long);
   const {
     data: { routes }
   } = await request.get('/routes', { params: { user_lat, user_long } });
