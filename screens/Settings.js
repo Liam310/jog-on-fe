0eaae7fa70c4f30811dd9146cddb3f4d7a909a4e
Simon Green <simonisaacgreen@icloud.com>
2019-11-04 16:34:46 +0000

    adding setting page title

--- screens/Settings.js
+++ screens/Settings.js
@@ -1,35 +1,52 @@
 import React from 'react';
 import { Text, View, Button } from 'react-native';
 import { NavigationActions } from 'react-navigation';
+import Constants from 'expo-constants';
 
 export default class Settings extends React.Component {
   render() {
     return (
-      <View style={{ flex: 1, justifyContent: 'center', alignItems: 'center' }}>
-        <Text>Settings</Text>
-        <Button
-          title="Miles"
-          onPress={() => {
-            this.props.navigation.dispatch(
-              NavigationActions.setParams({
-                params: { unit: 'miles' },
-                key: 'RouteList'
-              })
-            );
+      <>
+        <View
+          style={{
+            paddingTop: 10 + Constants.statusBarHeight,
+            paddingBottom: 10,
+            backgroundColor: '#ffffff'
           }}
-        />
-        <Button
-          title="Kilometres"
-          onPress={() => {
-            this.props.navigation.dispatch(
-              NavigationActions.setParams({
-                params: { unit: 'km' },
-                key: 'RouteList'
-              })
-            );
-          }}
-        />
-      </View>
+        >
+          <Text
+            style={{ fontSize: 18, textAlign: 'center', fontWeight: 'bold' }}
+          >
+            Settings
+          </Text>
+        </View>
+        <View
+          style={{ flex: 1, justifyContent: 'center', alignItems: 'center' }}
+        >
+          <Button
+            title="Miles"
+            onPress={() => {
+              this.props.navigation.dispatch(
+                NavigationActions.setParams({
+                  params: { unit: 'miles' },
+                  key: 'RouteList'
+                })
+              );
+            }}
+          />
+          <Button
+            title="Kilometres"
+            onPress={() => {
+              this.props.navigation.dispatch(
+                NavigationActions.setParams({
+                  params: { unit: 'km' },
+                  key: 'RouteList'
+                })
+              );
+            }}
+          />
+        </View>
+      </>
     );
   }
 }
