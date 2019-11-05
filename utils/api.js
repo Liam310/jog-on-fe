ebadd59f14935d53798914994307739b08ce768a
Dave Ridealgh <davecodingstuff@gmail.com>
2019-11-05 19:37:37 +0000

    added authentication between front and back end for get routes endpoint

--- utils/api.js
+++ utils/api.js
@@ -1,8 +1,9 @@
 import * as axios from 'axios';
+import { Auth } from 'aws-amplify';
 
 const request = axios.create({
-  baseURL: 'https://judsfg9bq1.execute-api.eu-west-2.amazonaws.com/latest/api'
-  // baseURL: 'http://172.26.44.25:3000/api'
+  // baseURL: 'https://judsfg9bq1.execute-api.eu-west-2.amazonaws.com/latest/api'
+  baseURL: 'http://172.26.44.25:3000/api'
 });
 
 // FLAGS
@@ -18,11 +19,35 @@ export const getFlags = async regionQueryObj => {
 };
 
 // ROUTES
-export const getRoutes = async ({ user_lat, user_long }) => {
-  const {
-    data: { routes }
-  } = await request.get('/routes', { params: { user_lat, user_long } });
-  return routes;
+// export const getRoutes = async ({ user_lat, user_long }) => {
+//   const {
+//     signInUserSession: {
+//       accessToken: { jwtToken }
+//     }
+//   } = await Auth.currentAuthenticatedUser();
+//   console.log(jwtToken);
+
+//   const {
+//     data: { routes }
+//   } = await request.get('/routes', {
+//     params: { user_lat, user_long },
+//     // headers: { userKey: jwtToken }
+//     headers: { userToken: jwtToken }
+//   });
+//   return routes;
+// };
+
+export const getRoutes = ({ user_lat, user_long }) => {
+  return Auth.currentAuthenticatedUser()
+    .then(({ signInUserSession: { accessToken: { jwtToken } } }) => {
+      return request.get('/routes', {
+        params: { user_lat, user_long },
+        headers: { usertoken: jwtToken }
+      });
+    })
+    .then(({ data: { routes } }) => {
+      return routes;
+    });
 };
 
 export const postRoute = route => {
