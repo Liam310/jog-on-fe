5061413dc3d88be597cc310b54c1be4fc9636b48
Simon Green <simonisaacgreen@icloud.com>
2019-11-06 18:56:41 +0000

    update KeyToggle positioning

--- components/KeyToggle.js
+++ components/KeyToggle.js
@@ -1,22 +1,22 @@
-import React from "react";
-import { TouchableHighlight, View } from "react-native";
-import Constants from "expo-constants";
-import { MaterialCommunityIcons } from "@expo/vector-icons";
+import React from 'react';
+import { TouchableHighlight, View } from 'react-native';
+import Constants from 'expo-constants';
+import { MaterialCommunityIcons } from '@expo/vector-icons';
 
 export default function KeyToggle({ handleToggle }) {
   return (
     <TouchableHighlight
       onPress={handleToggle}
       style={{
-        position: "absolute",
-        top: 15 + Constants.statusBarHeight,
+        position: 'absolute',
+        top: 15,
         left: 15,
         borderRadius: 500
       }}
     >
       <View
         style={{
-          backgroundColor: "#848484",
+          backgroundColor: '#848484',
           borderRadius: 500,
           paddingLeft: 15,
           paddingRight: 15,
@@ -25,8 +25,8 @@ export default function KeyToggle({ handleToggle }) {
           width: 70,
           height: 70,
           flex: 1,
-          justifyContent: "center",
-          alignItems: "center"
+          justifyContent: 'center',
+          alignItems: 'center'
         }}
       >
         <MaterialCommunityIcons name="key" size={36} color="white" />
