1bd25ba9ec3a33e265b2605ecff4148e767eff4d
Simon Green <simonisaacgreen@icloud.com>
2019-11-06 10:06:22 +0000

    add all routes and my routes functionality to routesList screen

--- screens/RouteList.js
+++ screens/RouteList.js
@@ -6,12 +6,26 @@ import RouteCard from '../components/RouteCard';
 import Constants from 'expo-constants';
 import * as polyline from '@mapbox/polyline';
 import * as Location from 'expo-location';
+import { ButtonGroup } from 'react-native-elements';
+import Amplify, { Auth } from 'aws-amplify';
 
 export default class RouteList extends React.Component {
   state = {
     routes: [],
     p: 1,
-    loading: true
+    loading: true,
+    selectedIndex: 0
+  };
+
+  buttons = ['All Routes', 'My Routes'];
+  updateIndex = () => {
+    // If selectedIndex was 0, make it 1.  If it was 1, make it 0
+    const newIndex = this.state.selectedIndex === 0 ? 1 : 0;
+    this.setState({ selectedIndex: newIndex }, () => {
+      this.state.selectedIndex === 0
+        ? this.getCurrentLocation()
+        : this.getCurrentLocationMyRoutes();
+    });
   };
 
   render() {
@@ -32,37 +46,81 @@ export default class RouteList extends React.Component {
             backgroundColor: '#ffffff'
           }}
         >
-          <Text
+          {/* <Text
             style={{ fontSize: 18, textAlign: 'center', fontWeight: 'bold' }}
           >
             Routes
-          </Text>
+          </Text> */}
+
+          <ButtonGroup
+            onPress={this.updateIndex}
+            selectedIndex={this.state.selectedIndex}
+            buttons={this.buttons}
+            selectedButtonStyle={{ backgroundColor: '#3cc1c7' }}
+          />
+
+          {this.state.selectedIndex === 0 ? (
+            <ScrollView
+              scrollEventThrottle="8"
+              onScroll={({ nativeEvent }) => {
+                if (
+                  isCloseToBottom(nativeEvent) &&
+                  this.state.loading === false
+                ) {
+                  this.handleBottom();
+                }
+              }}
+              showsVerticalScrollIndicator={false}
+              contentContainerStyle={{
+                paddingTop: 10,
+                paddingBottom: 60,
+                alignItems: 'center'
+              }}
+            >
+              <NavigationEvents onDidFocus={this.getCurrentLocation} />
+              {routes.map(route => {
+                return (
+                  <RouteCard
+                    key={route.route_id}
+                    route={route}
+                    distanceUnit={distanceUnit}
+                    handleRouteSelect={this.handleRouteSelect}
+                  />
+                );
+              })}
+            </ScrollView>
+          ) : (
+            <ScrollView
+              scrollEventThrottle="8"
+              onScroll={({ nativeEvent }) => {
+                if (
+                  isCloseToBottom(nativeEvent) &&
+                  this.state.loading === false
+                ) {
+                  this.handleBottom();
+                }
+              }}
+              showsVerticalScrollIndicator={false}
+              contentContainerStyle={{
+                paddingTop: 10,
+                paddingBottom: 60,
+                alignItems: 'center'
+              }}
+            >
+              <NavigationEvents onDidFocus={this.getCurrentLocationMyRoutes} />
+              {routes.map(route => {
+                return (
+                  <RouteCard
+                    key={route.route_id}
+                    route={route}
+                    distanceUnit={distanceUnit}
+                    handleRouteSelect={this.handleRouteSelect}
+                  />
+                );
+              })}
+            </ScrollView>
+          )}
         </View>
-        <ScrollView scrollEventThrottle="8"
-          onScroll={({ nativeEvent }) => {
-            if (isCloseToBottom(nativeEvent) && this.state.loading === false) {
-              this.handleBottom();
-            }
-          }}
-          showsVerticalScrollIndicator={false}
-          contentContainerStyle={{
-            paddingTop: 10,
-            paddingBottom: 60,
-            alignItems: 'center'
-          }}
-        >
-          <NavigationEvents onDidFocus={this.getCurrentLocation} />
-          {routes.map(route => {
-            return (
-              <RouteCard
-                key={route.route_id}
-                route={route}
-                distanceUnit={distanceUnit}
-                handleRouteSelect={this.handleRouteSelect}
-              />
-            );
-          })}
-        </ScrollView>
       </>
     );
   }
@@ -71,11 +129,15 @@ export default class RouteList extends React.Component {
     api.getRoutes({ ...currentLocation, p: this.state.p }).then(routes => {
       // console.log(routes);
       this.setState(currentState => {
-        let p = currentState.p
-        if (routes.length===0) p--
+        let p = currentState.p;
+        if (routes.length === 0) p--;
         newRoutes = [];
         routes.forEach(route => {
-          if (!currentState.routes.map(route=>route.route_id).includes(route.route_id)) {
+          if (
+            !currentState.routes
+              .map(route => route.route_id)
+              .includes(route.route_id)
+          ) {
             newRoutes.push(route);
           }
         });
@@ -85,7 +147,6 @@ export default class RouteList extends React.Component {
           p
         };
       });
-
     });
   };
 
@@ -97,12 +158,25 @@ export default class RouteList extends React.Component {
   };
 
   getCurrentLocation = async () => {
+    console.log('getting all routes');
     const initialLocation = await Location.getCurrentPositionAsync({});
     this.fetchRoutes({
       user_lat: initialLocation.coords.latitude,
       user_long: initialLocation.coords.longitude
     });
   };
+
+  getCurrentLocationMyRoutes = async () => {
+    console.log('getting my routes');
+    const initialLocation = await Location.getCurrentPositionAsync({});
+    const { username } = await Auth.currentAuthenticatedUser();
+    this.fetchRoutes({
+      user_id: username,
+      user_lat: initialLocation.coords.latitude,
+      user_long: initialLocation.coords.longitude
+    });
+  };
+
   handleBottom = () => {
     this.setState(
       currentState => {
