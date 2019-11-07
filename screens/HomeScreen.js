7a7fdf053232d43b894ee333575afd39fd915d33
Simon Green <simonisaacgreen@icloud.com>
2019-11-07 10:08:49 +0000

    set pitchEnabled to false to prevent the maps camera pitch angle being changed

--- screens/HomeScreen.js
+++ screens/HomeScreen.js
@@ -1,25 +1,20 @@
-import React from "react";
-import {
-  View,
-  Dimensions,
-  Platform,
-  ActivityIndicator
-} from "react-native";
-import MapView, { Polyline, Marker } from "react-native-maps";
-import Constants from "expo-constants";
-import * as Location from "expo-location";
-import * as Permissions from "expo-permissions";
-import * as api from "../utils/api";
-import flagRef from "../utils/flagRefObj";
+import React from 'react';
+import { View, Dimensions, Platform, ActivityIndicator } from 'react-native';
+import MapView, { Polyline, Marker } from 'react-native-maps';
+import Constants from 'expo-constants';
+import * as Location from 'expo-location';
+import * as Permissions from 'expo-permissions';
+import * as api from '../utils/api';
+import flagRef from '../utils/flagRefObj';
 import {
   checkSufficientRegionChange,
   convertRouteToRegion
-} from "../utils/utils";
-import ToggleFlags from "../components/ToggleFlags";
-import KeyToggle from "../components/KeyToggle";
-import KeyBox from "../components/KeyBox";
-import { NavigationEvents } from "react-navigation";
-import StartStop from "../components/StartStop";
+} from '../utils/utils';
+import ToggleFlags from '../components/ToggleFlags';
+import KeyToggle from '../components/KeyToggle';
+import KeyBox from '../components/KeyBox';
+import { NavigationEvents } from 'react-navigation';
+import StartStop from '../components/StartStop';
 
 export default class HomeScreen extends React.Component {
   state = {
@@ -56,35 +51,35 @@ export default class HomeScreen extends React.Component {
       keyModal,
       isLoading
     } = this.state;
-    const chosenRoute = navigation.getParam("decodedPoly", []);
+    const chosenRoute = navigation.getParam('decodedPoly', []);
     return (
       <View
         style={{
           flex: 1,
-          alignItems: "center",
-          justifyContent: "center",
+          alignItems: 'center',
+          justifyContent: 'center',
           paddingTop: Constants.statusBarHeight,
-          backgroundColor: "#ffffff"
+          backgroundColor: '#ffffff'
         }}
       >
         {isLoading ? (
           <View
             style={{
-              height: "100%",
-              width: "100%",
-              justifyContent: "center",
-              alignItems: "center"
+              height: '100%',
+              width: '100%',
+              justifyContent: 'center',
+              alignItems: 'center'
             }}
           >
-            <ActivityIndicator size={"large"} color="#3cc1c7" />
+            <ActivityIndicator size={'large'} color="#3cc1c7" />
           </View>
         ) : (
           <View
             style={{
               flex: 1,
-              alignItems: "center",
-              justifyContent: "center",
-              backgroundColor: "#ffffff"
+              alignItems: 'center',
+              justifyContent: 'center',
+              backgroundColor: '#ffffff'
             }}
           >
             <NavigationEvents
@@ -99,8 +94,9 @@ export default class HomeScreen extends React.Component {
               followsUserLocation={gettingLocation}
               rotateEnabled={false}
               showsUserLocation={true}
+              pitchEnabled={false}
               style={{
-                width: Dimensions.get("window").width,
+                width: Dimensions.get('window').width,
                 flex: 1
               }}
               region={mapRegion}
@@ -147,10 +143,10 @@ export default class HomeScreen extends React.Component {
   }
 
   componentWillMount() {
-    if (Platform.OS === "android" && !Constants.isDevice) {
+    if (Platform.OS === 'android' && !Constants.isDevice) {
       this.setState({
         errorMessage:
-          "Oops, this will not work on Sketch in an Android emulator. Try it on your device!"
+          'Oops, this will not work on Sketch in an Android emulator. Try it on your device!'
       });
     } else {
       this.getLocationAsync();
@@ -159,9 +155,9 @@ export default class HomeScreen extends React.Component {
 
   getLocationAsync = async () => {
     let { status } = await Permissions.askAsync(Permissions.LOCATION);
-    if (status !== "granted") {
+    if (status !== 'granted') {
       this.setState({
-        errorMessage: "Permission to access location was denied"
+        errorMessage: 'Permission to access location was denied'
       });
     }
 
@@ -247,7 +243,7 @@ export default class HomeScreen extends React.Component {
         } else {
           this.locationUpdateWatcher.remove();
           if (actualRoute.length > 3) {
-            this.props.navigation.navigate("FirstQuestion", {
+            this.props.navigation.navigate('FirstQuestion', {
               actualRoute,
               existingFlags
             });
